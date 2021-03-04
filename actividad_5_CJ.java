cee503d7682e55039dfb25f538de0e46185e48e1
javiercovat <74168368+javiercovat@users.noreply.github.com>
2021-03-03 17:51:44 -0700

    Add files via upload

---
+++ actividad_5_CJ.java
@@ -0,0 +1,30 @@
+public class actividad_5_CJ {
+    public static void main (String[] args){
+        //Declarar e imprimir variables
+        int q = 29;
+        float p = 3.1416f;
+        System.out.println("p = 29");
+        System.out.println("q = 3.1416");
+
+        // Suma
+        float suma = q + p;
+        System.out.println("El resultado de la operación 'q + p' es = " + suma);
+
+        // Resta
+        float resta = q - p;
+        System.out.println("El resultado de la operación 'q - p' es = " + resta);
+
+        // Multiplicación
+        double multiplicacion = q * p;
+        System.out.println("El resultado de la operación 'q * p' es = " + multiplicacion);
+
+        // División
+        double division = q / p;
+        System.out.println("El resultado de la operación 'q / p' es = " + division);
+
+        // Módulo (residuo division)
+        double modulo = q % p;
+        System.out.println("El resultado de la operación 'q % p' es = " + modulo);
+
+    }
+}
